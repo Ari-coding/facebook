7b1372cd89d685cd20bb8309d9f57ce70f508448
ari <ari_dia_dia@yahoo.com>
2025-10-17 09:14:28 +0300

    add login features

---
+++ components/login.js
@@ -0,0 +1,21 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form>
+        <label>
+          Username:
+          <input type="text" name="username" />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" />
+        </label>
+
+        <br />
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
